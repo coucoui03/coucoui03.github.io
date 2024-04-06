3b8dcf9698f12276fe577f7ea5cdfcab3334b1be
coucoui03 <1351172767@qq.com>
2024-04-06 19:54:11 +0800

    Site updated: 2024-04-06 19:54:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/undefined.html","posts/4a17b156.html"];function toRandomPost(){
+var posts=["posts/4a17b156.html","posts/undefined.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

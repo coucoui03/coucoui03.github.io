d7531ae54b849d84e43959ece0235878286825e0
coucoui03 <1351172767@qq.com>
2024-04-23 16:45:55 +0800

    Site updated: 2024-04-23 16:45:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/dfb8a81f.html","posts/undefined.html","posts/4a17b156.html","posts/dfb8a81f.html","posts/dfb8a81f.html","posts/dfb8a81f.html"];function toRandomPost(){
+var posts=["posts/dfb8a81g.html","posts/4a17b156.html","posts/undefined.html","posts/dfb8a81e.html","posts/dfb8a81f.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

381d85647e102a095c36a9a3e92472858d1bd14b
coucoui03 <1351172767@qq.com>
2024-04-23 16:54:40 +0800

    Site updated: 2024-04-23 16:54:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/8f66c494.html","posts/4a17b156.html","posts/undefined.html","posts/dfb8a81f.html"];function toRandomPost(){
+var posts=["posts/4a17b156.html","posts/8f66c494.html","posts/undefined.html","posts/dfb8a81f.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
